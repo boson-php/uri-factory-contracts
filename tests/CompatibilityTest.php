40ab28d999e5fae1ebb23f9eb71381c544caca01
SerafimArts <nesk@xakep.ru>
2025-07-29 14:06:52 +0000

    Add URI factory exception

--- tests/CompatibilityTest.php
+++ tests/CompatibilityTest.php
@@ -7,9 +7,9 @@ namespace Boson\Contracts\Uri\Factory\Tests;
 use Boson\Contracts\Uri\Component\PathInterface;
 use Boson\Contracts\Uri\Component\QueryInterface;
 use Boson\Contracts\Uri\Component\SchemeInterface;
-use Boson\Contracts\Uri\Factory\PathFactoryInterface;
-use Boson\Contracts\Uri\Factory\QueryFactoryInterface;
-use Boson\Contracts\Uri\Factory\SchemeFactoryInterface;
+use Boson\Contracts\Uri\Factory\Component\UriPathFactoryInterface;
+use Boson\Contracts\Uri\Factory\Component\UriQueryFactoryInterface;
+use Boson\Contracts\Uri\Factory\Component\UriSchemeFactoryInterface;
 use Boson\Contracts\Uri\Factory\UriFactoryInterface;
 use Boson\Contracts\Uri\UriInterface;
 use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
@@ -26,31 +26,31 @@ final class CompatibilityTest extends TestCase
     public function testUriFactoryInterfaceCompatibility(): void
     {
         new class implements UriFactoryInterface {
-            public function createUriFromString(string $uri): UriInterface {}
+            public function createUriFromString(string|\Stringable $uri): UriInterface {}
         };
     }
 
     #[DoesNotPerformAssertions]
     public function testSchemeFactoryInterfaceCompatibility(): void
     {
-        new class implements SchemeFactoryInterface {
-            public function createSchemeFromString(string $scheme): SchemeInterface {}
+        new class implements UriSchemeFactoryInterface {
+            public function createSchemeFromString(string|\Stringable $scheme): SchemeInterface {}
         };
     }
 
     #[DoesNotPerformAssertions]
     public function testQueryFactoryInterfaceCompatibility(): void
     {
-        new class implements QueryFactoryInterface {
-            public function createQueryFromString(string $query): QueryInterface {}
+        new class implements UriQueryFactoryInterface {
+            public function createQueryFromString(string|\Stringable $query): QueryInterface {}
         };
     }
 
     #[DoesNotPerformAssertions]
     public function testPathFactoryInterfaceCompatibility(): void
     {
-        new class implements PathFactoryInterface {
-            public function createPathFromString(string $path): PathInterface {}
+        new class implements UriPathFactoryInterface {
+            public function createPathFromString(string|\Stringable $path): PathInterface {}
         };
     }
 }
